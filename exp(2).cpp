82c95ee8a0afb4d51612cf3821915c5a453c36bf
vibhorkudeshiya <142561081+vibhorkudeshiya@users.noreply.github.com>
2023-10-27 19:42:05 +0530

    Create exp(2).cpp

---
+++ exp(2).cpp
@@ -0,0 +1,19 @@
+/*22070123132 Experiment 2*/
+#include<iostream>
+using namespace std;
+main()
+{
+int i = 45;
+float fl = 32.123;
+string f = "fruit";
+long l = 4513654654646.4554;
+double doub = 44566.4644644348964;
+cout<<i+fl<<endl;
+cout<<l - doub<<endl;
+cout<<"The size of int is "<<sizeof(i)<<" bytes"<<endl;
+cout<<"The size of int is "<<sizeof(fl)<<" bytes"<<endl;
+cout<<"The size of int is "<<sizeof(f)<<" bytes"<<endl;
+cout<<"The size of int is "<<sizeof(l)<<" bytes"<<endl;
+cout<<"The size of int is "<<sizeof(doub)<<" bytes"<<endl;
+return 0;
+}
